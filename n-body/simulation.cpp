e807edac6b4fa65e7aea10f827d7ead167b32e80
Puhinsky <puhinsky@gmail.com>
2024-05-06 23:46:32 +0300

    Fix accelerations

--- n-body/simulation.cpp
+++ n-body/simulation.cpp
@@ -44,6 +44,7 @@ void simulation::simulate()
 
 		for (size_t i = 0; i < m_bodies_count; i++)
 		{
+			m_bodies[i].m_acceleration = { 0.0, 0.0, 0.0 };
 			m_bodies[i].update_position_and_velocity(m_deltaTime);
 		}
 
